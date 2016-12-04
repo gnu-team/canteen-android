cc7fd2c51c27dc2aed125b90213d877d236c5e88
Jitae Kim <kimjt1226@gmail.com>
2016-12-03 21:41:45 -0500

    android version

---
+++ app/src/main/java/orgloginnext/httpscanteen_water/canteen/MainActivity.java
@@ -0,0 +1,32 @@
+package orgloginnext.httpscanteen_water.canteen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://canteen-water.org/login/?next=/");
+        mWebView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
